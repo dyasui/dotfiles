ee2462baef2238df7344b3c3a8a392d97ca5fffd
dante <dante@dantes-MacBook-Air.local>
2024-06-03 22:12:12 -0700

    added obsidian.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -211,4 +211,29 @@ return {
     },
   },
 
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+      preferred_link_style = "markdown"
+    },
+  },
+
 }
