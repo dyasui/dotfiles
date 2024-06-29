5bb484fb33378807b9f74bf7102cdff25780b505
Dante <dyasui@uoregon.edu>
2024-06-28 23:38:19 -0700

    added YankAssasin.nvim

--- nvim/lua/dante/plugins/init.lua
+++ nvim/lua/dante/plugins/init.lua
@@ -5,4 +5,13 @@ return {
     priority = 1000,
     config = true,
   },
+  {
+    "svban/YankAssassin.nvim",
+    config = function()
+      require("YankAssassin").setup {
+             auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+             auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+         }
+    end,
+  },
 }
