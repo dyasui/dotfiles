4d6d0fedfdf6369be5ed491e29de48b0abe04e0a
dyasui <dyasui@uoregon.edu>
2025-01-10 19:50:27 -0800

    R location for mac install

--- nvim/.config/nvim/lua/dante/plugins/r-nvim.lua
+++ nvim/.config/nvim/lua/dante/plugins/r-nvim.lua
@@ -3,7 +3,7 @@ return {
   version = "~0.1.0",
   config = function()
     vim.g.R_app = "~/Library/Python/3.9/bin/radian"
-    vim.g.R_cmd = "R"
+    vim.g.R_cmd = "/usr/local/bin/R"
     vim.g.R_external_term = "kitty"
     vim.g.R_editing_mode = "vi"
     vim.g.pdfviewer = "skim"
